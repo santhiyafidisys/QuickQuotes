9d7b9e7d2b85c31e169e7b8c02d2ef7b12ce535a
santhiya <santhiya2712@gmail.com>
2021-05-14 12:18:27 +0530

    changes in api connectivity

--- src/Get.js
+++ src/Get.js
@@ -2,10 +2,17 @@ import React from 'react';
 import styles from './rates.module.css';
 
 class GetQuote extends React.Component{
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    console.log('https://api.ofx.com/PublicSite.ApiService/OFX/spotrate/Individual/AUD/GBP/10000?format=json');
+  }
     render(){
         return(
           <div>
-            <button className={styles.button} onclick="href://api.ofx.com/PublicSite.ApiService/OFX/spotrate/individual/AUD/GBP/10000?format=json" type="button">Get Quote</button>    
+            <button className={styles.button} onClick={this.handleClick} type="button">Get Quote</button>    
             
 
           </div>
