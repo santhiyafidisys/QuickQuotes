9eca5dc3d12aff39ab41e24c2b821996fcc0ab12
santhiya <santhiya2712@gmail.com>
2021-05-20 18:14:45 +0530

    event handling

--- src/Telephone.js
+++ src/Telephone.js
@@ -2,6 +2,22 @@ import React from 'react';
 import styles from './rates.module.css';
 
 class Telephones extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {Telephone: ''};
+    this.handleChangePhone = this.handleChangePhone.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+
+  }
+
+handleChangePhone(event){
+  this.setState({ Telephone: event.target.value });
+ 
+  console.log('Telephone ' + this.state.Telephone);    
+}
+handleClick() {
+  console.log(this.state);
+}
     render(){
       return(
         <div>
@@ -13,7 +29,7 @@ class Telephones extends React.Component{
 							<option value="+91">+98</option>
 							<option value="+91">+80</option>
 						</select>
-            <input className={styles.textlongbox} type="tel" name=""></input>
+            <input className={styles.textlongbox} type="tel" onClick={this.handleClick} onChange={this.handleChangePhone}name="Telephone"></input>
 
         </div> 
       )
